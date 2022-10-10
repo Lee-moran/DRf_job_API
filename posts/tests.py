3af7f9d559b7416960e3d8ffe1ee90a768f64e23
Lee-moran <leemoran06@hotmail.com>
2022-10-10 14:55:35 +0000

    create comments app, added commentlist and commentdeatil to view

--- posts/tests.py
+++ posts/tests.py
@@ -7,17 +7,17 @@ from .models import Post
 # Create your tests here.
 class PostListViewTests(APITestCase):
     def setUp(self):
-        User.objects.create_user(username='adam', password='pass')
+        User.objects.create_user(username='lee', password='pass')
 
     def test_can_list_posts(self):
-        adam = User.objects.get(username='adam')
-        Post.objects.create(owner=adam, title='a title')
+        lee = User.objects.get(username='lee')
+        Post.objects.create(owner=lee, title='a title')
         response = self.client.get('/posts/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print(response.data, len(response.data))
 
     def test_logged_in_user_can_create_post(self):
-        self.client.login(username='adam', password='pass')
+        self.client.login(username='lee', password='pass')
         response = self.client.post('/posts/', {'title': 'a title'})
         count = Post.objects.count()
         self.assertEqual(count, 1)
@@ -30,13 +30,13 @@ class PostListViewTests(APITestCase):
 
 class PostDetailViewTests(APITestCase):
     def setUp(self):
-        adam = User.objects.create_user(username='adam', password='pass')
-        brian = User.objects.create_user(username='brian', password='pass')
+        lee = User.objects.create_user(username='lee', password='pass')
+        karl = User.objects.create_user(username='karl', password='pass')
         Post.objects.create(
-            owner=adam, title='a title', content='adams content'
+            owner=lee, title='a title', content='lees content'
         )
         Post.objects.create(
-            owner=brian, title='another title', content='brians content'
+            owner=karl, title='another title', content='karls content'
         )
     
     def test_can_retrieve_post_using_valid_id(self):
@@ -49,14 +49,14 @@ class PostDetailViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_user_can_update_own_post(self):
-        self.client.login(username='adam', password='pass')
+        self.client.login(username='lee', password='pass')
         response = self.client.put('/posts/1/', {'title': 'a new title'})
         post = Post.objects.filter(pk=1).first()
         self.assertEqual(post.title, 'a new title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_user_cant_update_another_users_post(self):
-        self.client.login(username='adam', password='pass')
+        self.client.login(username='lee', password='pass')
         response = self.client.put('/posts/2/', {'title': 'a new title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
