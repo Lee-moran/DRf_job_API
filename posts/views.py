293f5092ab47300ae8d9cd19f60684cce0164706
Lee-moran <leemoran06@hotmail.com>
2022-10-11 10:43:18 +0000

    added likes_count and comments_count to post view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from job_API.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -9,7 +10,18 @@ class PostList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count = Count('likes', distinct=True),
+        comments_count = Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes_created_at'
+    ]
     serializer_class = PostSerializer
 
 
@@ -20,3 +32,7 @@ class PostDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    queryset = Post.objects.annotate(
+        likes_count = Count('likes', distinct=True),
+        comments_count = Count('comment', distinct=True)
+    ).order_by('-created_at')
