b1a675c08f05899b715a9a09c02dcb577ee14b42
Lee-moran <leemoran06@hotmail.com>
2022-12-06 14:33:34 +0000

    deleted location app

--- location/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from location import views
-
-urlpatterns = [
-    path('location/', views.LocationList.as_view()),
-    path('location/<int:pk>/', views.LocationDetail.as_view())
-]
